44cd3b06462b4d71c679399431287e7addf50375
Lavender <susie204@naver.com>
2023-10-25 13:54:40 +0900

    라이트 색감변경, 맵뷰 서치바 플레이스홀더 글씨크기 줄임

--- MatRoad/Map/MapViewController.swift
+++ MatRoad/Map/MapViewController.swift
@@ -287,7 +287,7 @@ extension MapViewController: UISearchBarDelegate {
         searchBar.layer.cornerCurve = .continuous
         searchBar.searchTextField.textColor = UIColor(named: "textColor")
         if let textField = searchBar.value(forKey: "searchField") as? UITextField {
-            textField.font = UIFont(name: "KCC-Ganpan", size: 13.0)//UIFont.systemFont(ofSize: 13) //플레이스 홀더 글씨 크기
+            textField.font = UIFont(name: "KCC-Ganpan", size: 12.0)//UIFont.systemFont(ofSize: 13) //플레이스 홀더 글씨 크기
         }
         view.addSubview(searchBar)
         
